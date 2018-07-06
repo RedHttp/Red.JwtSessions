896aff3a07c1864b55b86d700fc10013096df347
rosenbjerg <rosenbjerg@users.noreply.github.com>
2018-07-06 17:18:06 +0200

    Initial commit

---
+++ JwtSessionSettings.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using JWT.Algorithms;
+
+namespace Red.JwtSessions
+{
+    public class JwtSessionSettings
+    {
+        /// <summary>
+        /// Algorithm used by Jwt-library. Defaults to HMAC-SHA256-A1
+        /// </summary>
+        public IJwtAlgorithm Algoritm { get; set; } = new HMACSHA256Algorithm();
+        
+        public TimeSpan SessionLength { get; }
+        public string Secret { get; }
+        
+      
+        public JwtSessionSettings(TimeSpan sessionLength, string secret)
+        {
+            SessionLength = sessionLength;
+            Secret = secret;
+        }
+    }
+}
---
+++ JwtSessions.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Threading.Tasks;
+using JWT;
+using JWT.Builder;
+using Newtonsoft.Json;
+using Red.Interfaces;
+
+namespace Red.JwtSessions
+{
+    public class JwtSessions<TSession> : IRedMiddleware, IRedWebSocketMiddleware
+    {
+        /// <summary>
+        /// Constructor for JwtSessions Middleware
+        /// </summary>
+        /// <param name="settings">Settings object</param>
+        public JwtSessions(JwtSessionSettings settings)
+        {
+            _settings = settings;
+        }
+        
+ 
+        private readonly JwtSessionSettings _settings;
+
+        /// <summary>
+        ///     Do not invoke. Is invoked by the server when it starts. 
+        /// </summary>
+        public void Initialize(RedHttpServer server)
+        {
+            server.Plugins.Register(this);
+        }
+
+        /// <summary>
+        ///     Do not invoke. Is invoked by the server with every websocket request
+        /// </summary>
+        public async Task Process(Request req, WebSocketDialog wsd, Response res)
+        {
+            string token = null;
+            string auth = req.Headers["Authorization"];
+
+            if (string.IsNullOrEmpty(auth))
+            {
+                return;
+            }
+
+            if (auth.StartsWith("Bearer ", StringComparison.OrdinalIgnoreCase))
+            {
+                token = auth.Substring("Bearer ".Length).Trim();
+            }
+            
+            if (string.IsNullOrEmpty(token) || !TryAuthenticateToken(token, out var session))
+            {
+                return;
+            }
+            
+            req.SetData(session.Data);
+        }
+
+        /// <summary>
+        ///     Do not invoke. Is invoked by the server with every request
+        /// </summary>
+        public async Task Process(Request req, Response res)
+        {
+            string token = null;
+            string auth = req.Headers["Authorization"];
+
+            if (string.IsNullOrEmpty(auth))
+            {
+                return;
+            }
+
+            if (auth.StartsWith("Bearer ", StringComparison.OrdinalIgnoreCase))
+            {
+                token = auth.Substring("Bearer ".Length).Trim();
+            }
+          
+            if (string.IsNullOrEmpty(token) || !TryAuthenticateToken(token, out var session))
+            {
+                return;
+            }
+            
+            req.SetData(session.Data);
+        }
+        
+        private bool TryAuthenticateToken(string authorization, out JwtSession data)
+        {
+            try
+            {
+                var json = new JwtBuilder()
+                    .WithSecret(_settings.Secret)
+                    .WithAlgorithm(_settings.Algoritm)
+                    .MustVerifySignature()
+                    .Decode(authorization);
+                data = JsonConvert.DeserializeObject<JwtSession>(json);
+                return true;
+            }
+            catch (SignatureVerificationException)
+            {
+                data = null;
+                return false;
+            }
+            catch (JsonSerializationException e)
+            {
+                data = null;
+                return false;
+            }
+        }
+        
+        internal string NewSession(TSession sessionData)
+        {
+            var token = new JwtBuilder()
+                .WithSecret(_settings.Secret)
+                .WithAlgorithm(_settings.Algoritm)
+                .AddClaim("exp", DateTimeOffset.UtcNow.Add(_settings.SessionLength).ToUnixTimeSeconds())
+                .AddClaim("data", sessionData)
+                .Build();
+            
+            return $"Bearer {token}";
+        }
+
+        internal class JwtSession
+        {
+            public TSession Data;
+        }
+
+        
+    }
+}
